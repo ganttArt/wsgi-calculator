ee7cdef7a99ab79e249fb89db8d3a2de11fa2671
ganttArt <gantt.art@gmail.com>
2019-10-15 14:36:30 -0700

    Calculator completed, tests passing

--- calculator.py
+++ calculator.py
@@ -41,44 +41,91 @@ To submit your homework:
 
 """
 
-
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
+    return str(args[0][0] + args[0][1])
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+def subtract(*args):
+    ''' Returns a STRING with the value arg1 - arg2 '''
+    return str(args[0][0] - args[0][1]) 
 
-    return sum
+def divide(*args):
+    """ Returns a STRING with the value arg1 / arg2 """
+    return str(args[0][0] / args[0][1])
 
-# TODO: Add functions for handling more arithmetic operations.
+def multiply(*args):
+    ''' Returns a STRING with the value arg1 * arg2 '''
+    return str(args[0][0] * args[0][1])
+
+def home(*args):
+    ''' Returns a STRING with a description of how to use the calculator'''
+    description = """
+<h1>How to use the Calculator</h1>
+<body>http://localhost:8080/multiply/3/5   => 15<br>
+  http://localhost:8080/add/23/42      => 65<br>
+  http://localhost:8080/subtract/23/42 => -19<br>
+  http://localhost:8080/divide/22/11   => 2<br>
+  http://localhost:8080/               => this page<br>
+
+</body>
+    """
+    return description
 
 def resolve_path(path):
     """
     Should return two values: a callable and an iterable of
     arguments.
     """
+    funcs = {
+        '': home,
+        'add': add,
+        'multiply': multiply,
+        'subtract': subtract,
+        'divide': divide
+    }
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    
+    if func_name == '':
+        args = None
+    else:
+        args = [int(path[1]), int(path[2])]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
 
     return func, args
 
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    headers = [('Content-type', 'text/html')]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        print(path)
+        func, args = resolve_path(path)
+        body = func(args)
+        status = "200 OK"
+    except ZeroDivisionError:
+        status = "500 Internal Server Error"
+        body = "<h1>DO NOT DIVIDE BY 0!!!</h1>"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
